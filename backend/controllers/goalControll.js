308848e87e88467ad8104512849e35bd77b9d117
acutecode2 <acutefiles2@gmail.com>
2024-02-09 21:43:28 +0530

    async handler added

--- backend/controllers/goalControll.js
+++ backend/controllers/goalControll.js
@@ -1,26 +1,27 @@
-
+const asyncHandler = require('express-async-handler')
 /* Get Goals*/
-const getGoals = (req,res) =>{
+const getGoals = asyncHandler(async(req,res) =>{
     if(!req.body.text){
-        res.status(500).json({message : 'please add field'})
+        res.status(500)
+        throw new Error('please fill the field')
     }
     res.status(200).json({message : 'get goals'})
-    } 
+    }) 
 
 /* Create Goals*/
-const createGoals = (req,res) =>{
+const createGoals = asyncHandler(async(req,res) =>{
     res.status(200).json({message : 'create goals'})
-    } 
+    } )
 
 /* Get Goals*/
-const updateGoals = (req,res) =>{
+const updateGoals = asyncHandler(async(req,res) =>{
     res.status(200).json({message : `update goal ${req.params.id}`})
-    } 
+    }) 
 
 /* Get Goals*/
-const deleteGoals = (req,res) =>{
+const deleteGoals = asyncHandler(async(req,res) =>{
     res.status(200).json({message : `delete goal ${req.params.id}`})
-    } 
+    }) 
 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const {errorhandler} = require('./middleware/errhandle')
 
 const app = express()
 
 app.use(express.urlencoded({extended :false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorhandler)
 app.listen(port, () => console.log(`Server started on Port ${port}`))
